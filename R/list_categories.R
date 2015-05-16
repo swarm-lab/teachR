79c2f5a1eba81aa853c67edc8bae6636466ddb1b
Simon Garnier <simon.garnier@gmail.com>
2015-05-16 13:40:16 -0400

    Added functions to list all categories and keywords.

---
+++ R/list_categories.R
@@ -0,0 +1,37 @@
+#' List the categories of all apps available through \code{\link[<teachR>:<teachR>-package]{teachR}}
+#'
+#' This function generates a listing  of all the categories of apps available through 
+#' the \code{\link[<teachR>:<teachR>-package]{teachR}} library. 
+#' 
+#' @return A vector of character strings, each of them corresponding to a different
+#' category.
+#' 
+#' @author Simon Garnier: \email{garnier@@njit.edu}, \link[https://twitter.com/sjmgarnier]{@@sjmgarnier}
+#' 
+#' @examples
+#' list_categories()
+#' 
+#' @export
+#'
+list_categories <- function() {  
+  apps <- dir(paste0(find.package("teachR"), "/apps"))
+  info_table <- data.frame()
+  
+  for (i in 1:length(apps)) {
+    info <- read.table(paste0(find.package("teachR"), "/apps/", apps[i], "/info"), sep = ":")
+    info <- t(as.matrix(info)[,2])
+    info_table <- rbind(info_table, info)
+  }
+  info_table[] <- lapply(info_table, as.character)
+  names(info_table) <- c("Title", "Version", "Date", "Author", 
+                         "Maintainer", "Description", "Category", 
+                         "Keywords", "License", "Depends", "Command" )
+  
+  categories <- strsplit(info_table$Category, ",")
+  categories <- sort(unique(unlist(categories)))
+  sub(" ", "", categories)
+}
+
+
+
+
